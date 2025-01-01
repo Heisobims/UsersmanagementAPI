32f56a4d54b1db1b996d9a5dbeea1273d68a20b7
heisobims <heisobims@gmail.com>
2025-01-01 13:55:58 +0100

    added comments

--- UserDataBaseApi.py
+++ UserDataBaseApi.py
@@ -4,8 +4,10 @@ from typing import Optional
 from pydantic import BaseModel
 import uuid
 
+# Create an instance of FastAPI
 app = FastAPI()
 
+# Create a class that inherits from BaseModel
 class User(BaseModel):
     id: str = str(uuid.uuid4())
     student_name: str
@@ -14,6 +16,7 @@ class User(BaseModel):
     student_country: str
     student_email: str
 
+# Create a dictionary to store the students
 students_repo = {
     "Stanley": {
         "id": str(uuid.uuid4()),
@@ -65,16 +68,19 @@ students_repo = {
     },
 }
 
+# Create a route to get all the students
 @app.get("/")
 async def homepage():
     return students_repo
 
+# Create a route to get a specific student
 @app.get("/students/{student_name}")
 async def get_student(student_name: str = Path(..., title="The name of the student you want to get", description="The name of the student you want to get", example="Stanley")):
     if student_name in students_repo:
         return students_repo[student_name]
     return {"message": "Student not found"}
 
+# Create a route to create a new student
 @app.post("/createstudent")
 async def create_new_student(user: User) -> dict:
     if user.student_name in students_repo:
@@ -83,7 +89,8 @@ async def create_new_student(user: User) -> dict:
         user.id = str(uuid.uuid4())
         students_repo[user.student_name] = user.dict()
         return students_repo
-    
+
+# Create a route to update a student    
 @app.put("/updatestudent/{student_name}")
 async def update_student(user: User, student_name: str = Path(..., title="The name of the student you want to update", description="The name of the student you want to update", example="Stanley")) -> dict:
     if student_name in students_repo:
@@ -91,7 +98,9 @@ async def update_student(user: User, student_name: str = Path(..., title="The na
         return students_repo
     else:
         return {"message": "Student not found"}
-    
+
+
+ # Create a route to delete a student   
 @app.delete("/deletestudent/{student_name}")
 async def delete_student(student_name: str = Path(..., title="The name of the student you want to delete", description="The name of the student you want to delete", example="Stanley")) -> dict:
     if student_name in students_repo:
